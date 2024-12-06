4987991445434ea757b72e2146fe04a3b157bfd1
jake power <jakejosephpower@gmail.com>
2024-12-06 23:48:10 +0000

    list saves will return a saved game and its details like genre, rating etc

--- src/main/kotlin/models/Game.kt
+++ src/main/kotlin/models/Game.kt
@@ -1,32 +1,34 @@
 package ie.setu.models
 
+import utils.Utilities
+
 data class Game(
     var gameId: Int = 0,
     var gameName: String,
     var gameRating: Int,
     var gameCategory: String,
     var isGameSaved: Boolean = false,
-    var saves: MutableSet<Savefile> = mutableSetOf(),
-){
+    var saves: MutableSet<Savefile> = mutableSetOf()
+) {
     private var lastSaveId = 0
     private fun getSaveId() = lastSaveId++
 
-    fun addSave(save: Savefile): Boolean{
+    fun addSave(save: Savefile): Boolean {
         save.saveId = getSaveId()
         return saves.add(save)
     }
 
     fun amountOfSaves() = saves.size
 
-    fun findOne(id: Int): Savefile?{
-        return saves.find { savefile -> savefile.saveId == id  }
+    fun findOne(id: Int): Savefile? {
+        return saves.find { savefile -> savefile.saveId == id }
     }
 
-    fun delete(id: Int): Boolean{
+    fun delete(id: Int): Boolean {
         return saves.removeIf { savefile -> savefile.saveId == id }
     }
 
-    fun update( id: Int, newSavefile: Savefile) : Boolean{
+    fun update(id: Int, newSavefile: Savefile): Boolean {
         val foundSavefile = findOne(id)
 
         if (foundSavefile != null) {
@@ -37,4 +39,12 @@ data class Game(
         return false
     }
 
+    fun listSaves() =
+        if (saves.isEmpty()) "\tPlays have yet to be added, get to it!"
+        else Utilities.formatSetString(saves)
+
+    override fun toString(): String {
+        val archived = if (isGameSaved) 'Y' else 'N'
+        return "$gameId: $gameName, Rating($gameRating), Genre($gameCategory, Game saved($isGameSaved) \n${listSaves()}"
+    }
 }
